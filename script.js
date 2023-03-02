6c8487ee1ff8c8ecc75e7c9f00445fe0f179225c
nnic <104588184+nicholasqy@users.noreply.github.com>
2023-03-02 11:11:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// Get the dropdown button and content elements
-var dropdownBtn = document.querySelector(".dropbtn");
+// Get content
+var dropdownButton = document.querySelector(".dropdown-button");
 var dropdownContent = document.querySelector(".dropdown-content");
 
-// Add a click event listener to the dropdown button
-dropdownBtn.addEventListener("click", function() {
-  // Toggle the visibility 
-  if (dropdownContent.style.display === "block") {
-    dropdownContent.style.display = "none";
-  } else {
-    dropdownContent.style.display = "block";
-  }
+// click event listener to the button
+dropdownButton.addEventListener("click", function() {
+  // Toggle dropdown container
+  dropdownContent.parentElement.classList.toggle("active");
 });
