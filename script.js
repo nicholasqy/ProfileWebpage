03f35c021beeae0c75813e955f4a0d30892787a2
nnic <104588184+nicholasqy@users.noreply.github.com>
2023-03-03 10:43:38 -0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Get content
-var dropdownButton = document.querySelector(".dropdown-button");
-var dropdownContent = document.querySelector(".dropdown-content");
-
-// click event listener to the button
-dropdownButton.addEventListener("click", function() {
-  // Toggle dropdown container
-  dropdownContent.parentElement.classList.toggle("active");
-});
